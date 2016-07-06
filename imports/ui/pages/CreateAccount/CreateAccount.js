dbcee40bec68242245fbc20c218f1a722aa16bc8
Santiago Melo <smeloa@yahoo.com>
2016-07-06 15:15:15 -0400

    Began work on account creation

--- imports/ui/pages/CreateAccount/CreateAccount.js
+++ imports/ui/pages/CreateAccount/CreateAccount.js
@@ -1,2 +1,33 @@
 import './CreateAccount.html';
 import './CreateAccount.css';
+
+import { $ } from 'meteor/jquery';
+import { Meteor } from 'meteor/meteor';
+import { Template } from 'meteor/templating';
+import { FlowRouter } from 'meteor/kadira:flow-router';
+
+Template.CreateAccount.events({
+  'click .back'(event, instance) {
+    event.preventDefault();
+    FlowRouter.go('/')
+  },
+
+  'click .guide'(event, instance) {
+    event.preventDefault();
+    $('.guide').addClass('on');
+    $('#registerExplorerForm').addClass('hidden');
+    $('.explorer').removeClass('on');
+    $('#registerGuideForm').removeClass('hidden');
+    setTimeout(function() {
+		    Bert.alert('parcero');
+		}, 750);
+  },
+
+  'click .explorer'(event, instance) {
+    event.preventDefault();
+    $('.explorer').addClass('on');
+    $('#registerGuideForm').addClass('hidden');
+    $('.guide').removeClass('on');
+    $('#registerExplorerForm').removeClass('hidden');
+  },
+});
--- imports/ui/pages/Start.js
+++ imports/ui/pages/Start.js
@@ -1,6 +1,11 @@
 import './Start.html';
 import './Start.css';
 
+import { $ } from 'meteor/jquery';
+import { Meteor } from 'meteor/meteor';
+import { Template } from 'meteor/templating';
+import { FlowRouter } from 'meteor/kadira:flow-router';
+
 Template.Start.events({
   'click .create-account'(event, instance) {
     event.preventDefault();
