47cff6892d881c2b0fa9c81c275d477bdf7b8d0b
Santiago Melo <smeloa@yahoo.com>
2016-07-12 12:10:39 -0400

    minor import fix on methods

--- server/methods.js
+++ server/methods.js
@@ -1,3 +1,7 @@
+import { Roles } from 'meteor/alanning:roles';
+import { Meteor } from 'meteor/meteor';
+import { SimpleSchema } from 'meteor/aldeed:simple-schema';
+
 Meteor.methods({
   'addUserGuideRole'(userId){
     Roles.addUsersToRoles(userId, 'guide');
