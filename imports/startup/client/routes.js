464efcb70c2bb96dc934b6b089daf83842cc5cc1
Santiago Melo <smeloa@yahoo.com>
2016-07-13 13:59:40 -0500

    Setup routing and logout function

--- imports/startup/client/routes.js
+++ imports/startup/client/routes.js
@@ -2,6 +2,7 @@ import { Meteor } from 'meteor/meteor';
 import { Accounts } from 'meteor/accounts-password';
 import { FlowRouter } from 'meteor/kadira:flow-router';
 import { BlazeLayout } from 'meteor/kadira:blaze-layout';
+import { FlowRouterHelpers } from 'meteor/arillo:flow-router-helpers';
 import { ReactiveDict } from 'meteor/reactive-dict';
 import { Bert } from 'meteor/themeteorchef:bert';
 import { Roles } from 'meteor/alanning:roles'
@@ -11,6 +12,9 @@ import '../../ui/layouts/MainLayout.js';
 import '../../ui/pages/Start.js';
 import '../../ui/pages/CreateAccount/CreateAccount.js';
 import '../../ui/pages/Home/Home.js';
+import '../../ui/pages/MyTours/MyTours.js';
+import '../../ui/pages/Profile/Profile.js';
+import '../../ui/pages/CreateTour/CreateTour.js';
 
 //Frontend routes
 
@@ -44,6 +48,43 @@ FlowRouter.route('/home', {
     action(params) {
       if(Meteor.loggingIn() || Meteor.userId()){
         BlazeLayout.render("MainLayout", { content: "Home" });
+      } else {
+        FlowRouter.go("/");
+      }
+    }
+});
+
+
+FlowRouter.route('/mytours', {
+    name: 'mytours',
+    title: 'My Tours',
+    action(params) {
+      if(Meteor.loggingIn() || Meteor.userId()){
+        BlazeLayout.render("MainLayout", { content: "MyTours" });
+      } else {
+        FlowRouter.go("/");
+      }
+    }
+});
+
+FlowRouter.route('/profile', {
+    name: 'profile',
+    title: 'Profile',
+    action(params) {
+      if(Meteor.loggingIn() || Meteor.userId()){
+        BlazeLayout.render("MainLayout", { content: "Profile" });
+      } else {
+        FlowRouter.go("/");
+      }
+    }
+});
+
+FlowRouter.route('/createtour', {
+    name: 'createtour',
+    title: 'Create Tour',
+    action(params) {
+      if(Meteor.loggingIn() || Meteor.userId()){
+        BlazeLayout.render("MainLayout", { content: "CreateTour" });
       } else {
         FlowRouter.go("/");
       }
---
+++ imports/ui/pages/MyTours/MyTours.js
@@ -0,0 +1,11 @@
+import './MyTours.html';
+import './MyTours.css';
+
+import '../../components/NavBar/NavBar.js';
+
+import { $ } from 'meteor/jquery';
+import { Meteor } from 'meteor/meteor';
+import { Template } from 'meteor/templating';
+import { FlowRouter } from 'meteor/kadira:flow-router';
+import { Roles } from 'meteor/alanning:roles';
+import { ReactiveDict } from 'meteor/reactive-dict';
