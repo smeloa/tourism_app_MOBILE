ab155c95e798727f408257f84e649f560eef16c6
Santiago Melo <smeloa@yahoo.com>
2016-07-13 14:00:22 -0500

    minor fix

--- imports/ui/components/NavBar/NavBar.js
+++ imports/ui/components/NavBar/NavBar.js
@@ -1,3 +1,28 @@
 import './NavBar.html'
 
-import { Roles } from 'meteor/alanning:roles'
+import { Meteor } from 'meteor/meteor';
+import { Bert } from 'meteor/themeteorchef:bert';
+import { $ } from 'meteor/jquery';
+import { Roles } from 'meteor/alanning:roles';
+import { Template } from 'meteor/templating';
+import { FlowRouter } from 'meteor/kadira:flow-router';
+import { FlowRouterHelpers } from 'meteor/arillo:flow-router-helpers';
+
+Template.NavBar.events({
+	'click .logout'(event){
+		Meteor.logout();
+		FlowRouter.go('/');
+		setTimeout(function() {
+			Bert.alert("Keep exploring!", 'success');
+		}, 500);
+	}
+});
+
+Template.NavBar.helpers({
+	isSelectedItem(name) {
+		if(FlowRouter.getRouteName() == name) {
+			return 'active';
+		}
+		return null;
+	}
+})
