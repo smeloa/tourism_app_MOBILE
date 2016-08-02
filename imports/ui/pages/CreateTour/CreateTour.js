6b164545ad2aadecc8e9ef4a6ca307a77f706b3e
Santiago Melo <smeloa@yahoo.com>
2016-08-02 15:40:54 -0400

    Work on CreateTour form so that a tour is created

--- imports/ui/pages/CreateTour/CreateTour.js
+++ imports/ui/pages/CreateTour/CreateTour.js
@@ -9,6 +9,7 @@ import { Template } from 'meteor/templating';
 import { FlowRouter } from 'meteor/kadira:flow-router';
 import { Roles } from 'meteor/alanning:roles';
 import { ReactiveDict } from 'meteor/reactive-dict';
+import { Bert } from 'meteor/themeteorchef:bert';
 
 import { Cities } from '../../../api/cities/cities.js';
 
@@ -16,6 +17,35 @@ Template.CreateTour.onCreated(function eventCreateOnCreated() {
 	this.autorun((v) => {
 		this.subscribe('cities');
 	});
+});
+
+Template.CreateTour.events({
+	'submit #createTourForm'(event) {
+		event.preventDefault();
+
+		const target = event.target;
+
+		const newtour = {
+			tourname: target.tourname.value,
+			city: target.city.value,
+			date: target.date.value,
+			starttime: target.starttime.value,
+			timelength: target.timelength.value,
+			tourdescription: target.tourdescription.value
+		}
+
+		Meteor.call("tours.create", newtour, (error, result) => {
+			if(error){
+				console.log("error", error);
+			} else {
+				target.reset();
+				setTimeout(function() {
+					Bert.alert("Your tour has been created!", 'success');
+				}, 500);
+				FlowRouter.go('/home');
+			}
+		});
+	}
 });
 
 Template.CreateTour.helpers({
